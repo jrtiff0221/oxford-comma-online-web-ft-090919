1b1583bd4e8068fabc361306ab6f431c5199ae20
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-14 02:06:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
 def oxford_comma(array)
   if array.length == 1 then
     return array.first 
-  elsif arr
+  elsif array.length == 2 then  
+    return "#{array.first} and #{array.last}" 
   
   return "#{array[0...-1].join(", ")}, and #{array.last}"
 end
